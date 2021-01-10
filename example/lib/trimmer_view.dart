fb5b2f79b3a0a639515a19ff535f7d19e1b44c56
umesh <stha.ums@gmail.com>
2021-01-10 09:49:27 +0545

    changes as i see fit

--- example/lib/trimmer_view.dart
+++ example/lib/trimmer_view.dart
@@ -1,4 +1,3 @@
-import 'package:example/preview.dart';
 import 'package:flutter/material.dart';
 import 'package:video_trimmer/video_trimmer.dart';
 
@@ -14,7 +13,7 @@ class _TrimmerViewState extends State<TrimmerView> {
   double _endValue;
 
   // bool _isPlaying = false;
-  bool _progressVisibility = false;
+  // bool _progressVisibility = false;
 
   // Future<String> _saveVideo() async {
   //   setState(() {
